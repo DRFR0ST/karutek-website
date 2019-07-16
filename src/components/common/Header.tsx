fd453d86b3e886916a09cfee7682af9e1a19f880
Mike Eling <mike.eling97@gmail.com>
2019-07-17 01:30:58 +0200

    :bento: Add header component

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,21 +1,89 @@
 import React from 'react'
-import { withStyles, createStyles, Theme } from '@material-ui/core'
+import {
+  withStyles,
+  createStyles,
+  Theme,
+  Button,
+  Typography,
+} from '@material-ui/core'
 
 interface PropTypes {
   classes: any
 }
 
 const Header = ({ classes }: PropTypes) => {
-  return <div className={classes.root}>Header</div>
+  return (
+    <div className={classes.root}>
+      <div>
+        <Typography variant="h1" className={classes.logo}>
+          KARUTEK
+        </Typography>
+        <Typography paragraph className={classes.slogan}>
+          mesmerizing cool slogan here
+        </Typography>
+        <div className={classes.menu}>
+          <Button
+            className={classes.menuItem}
+            variant="outlined"
+            color="primary">
+            About me
+          </Button>
+          <Button
+            className={classes.menuItem}
+            variant="outlined"
+            color="secondary">
+            Experience
+          </Button>
+          <Button
+            className={classes.menuItem}
+            variant="outlined"
+            color="primary">
+            Contact
+          </Button>
+          <Button
+            className={classes.menuItem}
+            variant="outlined"
+            color="secondary">
+            Galery
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 const styles = ({ palette, spacing }: Theme) =>
   createStyles({
     root: {
       display: 'flex',
-      flexDirection: 'column',
-      backgroundColor: palette.background.default,
+      justifyContent: 'center',
+      alignItems: 'flex-end',
+      height: '30vh',
+      padding: '0 20vw',
+      textAlign: 'center',
+    },
+    logo: {
       color: palette.primary.main,
+      fontWeight: 'bold',
+      fontSize: '3.5rem',
+      margin: '0 !important',
+      fontFamily: "'Syncopate'",
+    },
+    slogan: {
+      margin: '0em 0 1rem 0',
+      opacity: 0.25,
+      fontSize: '1.25rem',
+      color: '#000',
+    },
+    menu: {
+      borderTop: '1px solid #eee',
+      padding: '1rem 0',
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+    menuItem: {
+      margin: '0.5rem',
     },
   })
 
